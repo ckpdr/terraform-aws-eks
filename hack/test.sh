ee13cb91b4c7f048cbb382353958e9c7699d208e
Manuel Pereira <manuel-pereira@cookpad.com>
2021-12-01 08:46:31 +0000

    Setup interview
    Reference: https://github.com/cookpad/global-web-platform/pull/348

--- hack/test.sh
+++ hack/test.sh
@@ -7,5 +7,12 @@ source hack/assume_role.sh
 
 export SKIP_cleanup_terraform=true
 
+# export SKIP_deploy_cluster=true
+# export SKIP_validate_vpc=true
+# export SKIP_validate_cluster=true
+export SKIP_validate_standard_node_group=true
+export SKIP_validate_bottlerocket_node_group=true
+export SKIP_validate_gpu_node_group=true
+
 cd test
 go test -v -timeout 90m -run TestTerraformAwsEksCluster | grep -v "constructing many client instances from the same exec auth config"
